56e9bcfd9baced9472533a3a1f4e1f8ea159176f
Derek Villars and Nicole Sanders <derek+nicole@>
2017-02-15 10:42:08 -0800

    add the first test

--- Objects/FindReplace.cs
+++ Objects/FindReplace.cs
@@ -5,5 +5,20 @@ namespace FindReplaceApp
 {
     public class FindReplace
     {
+        [Fact]
+        public void Replace_ReplaceWord_NewString()
+        {
+            //Arrange
+            string sentence = "Hello world";
+            string findWord = "world";
+            string replaceWord = "universe";
+
+            //Act
+            FindReplace findReplaceTest = new FindReplace(sentence, findWord, replaceWord);
+            string result = findReplaceTest.Replace();
+
+            //Assert
+            Assert.Equal("Hello universe", result);
+        }
     }
 }
